f7e3d0092132f58014dbe3ca03db673056c0ceb4
Alan Meigs <drakosfire@gmail.com>
2025-05-22 09:41:49 -0600

    sse client to handle broadcasts to client from backend

--- api/server/sseClients.js
+++ api/server/sseClients.js
@@ -5,7 +5,7 @@ const clients = new Map(); // userId -> Set of res
 function addClient(userId, res) {
     if (!clients.has(userId)) clients.set(userId, new Set());
     clients.get(userId).add(res);
-    logger.info(`[SSE] Added client for user: ${userId}, total: ${clients.get(userId).size}`);
+    // logger.info(`[SSE] Added client for user: ${userId}, total: ${clients.get(userId).size}`);
 }
 
 function removeClient(userId, res) {
@@ -20,7 +20,7 @@ function broadcastToUser(userId, event, data) {
         logger.info(`[SSE] No clients found for user: ${userId}`);
         return;
     }
-    logger.info(`[SSE] Broadcasting to user: ${userId}, client count: ${clients.get(userId).size}`);
+    logger.info(`[SSE] Broadcasting to user`);
     for (const res of clients.get(userId)) {
         logger.info(`[SSE] Writing event to user: ${userId}`);
         res.write(`event: ${event}\ndata: ${JSON.stringify(data)}\n\n`);
